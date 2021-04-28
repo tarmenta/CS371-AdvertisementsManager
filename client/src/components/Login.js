86a5d635dc6497b68ab0f0b0a6b28f834c453748
tarmenta <51331495+tarmenta@users.noreply.github.com>
2021-04-28 00:36:21 -0500

    Login done

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,24 +1,46 @@
-import React from 'react'
-import { Input, Grid, Form, Button } from "semantic-ui-react";
+import React, {useState} from 'react'
+import { Form, Button } from "semantic-ui-react";
 
 
 
 const Login = () => {
-    
+    const [login_user, setUser] = useState("");
+    const[login_password, setPass] = useState("");
     
     
     return (
         <Form >
         <Form.Field>
           <label>Login</label>
-          <Form.Input placeholder= "UserName"
+          <Form.Input placeholder= "UserName" value={login_user}
+            onChange={(e) => setUser(e.target.value)}></Form.Input>
+        </Form.Field>
 
-        </Form.Field>
         <Form.Field>
-          <input placeholder='Password' />
+          <Form.Input placeholder='Password' value={login_password}
+            onChange={(e) => setPass(e.target.value)}></Form.Input>>
         </Form.Field>
+
         <Form.Field>
-          <Button type='submit'>Submit</Button>
+          <Button type='submit' 
+          onClick={async () => {
+            const user = {
+              login_user,
+              login_password,
+            };
+            await fetch("http://localhost:5000/authentication", {
+              method: "POST",
+              header: {
+                "Content-Type": "applicatin/json",
+
+              },
+              body: JSON.stringify(user),
+            });
+            window.location.reload();
+          }}
+          >
+            Submit
+            </Button>
         </Form.Field>
         
         <Button primary>Create New User</Button>
